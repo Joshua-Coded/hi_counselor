2726563f3a1051b9b8d7671a3c1e5bd3a5b8349d
joshbam-joshwilly <j.alana@alustudent.com>
2023-12-28 16:20:48 +0100

    binary tree node added

---
+++ main/BinaryTreeNode.java
@@ -0,0 +1,12 @@
+public class BinaryTreeNode {
+    public int data;
+    public BinaryTreeNode left;
+    public BinaryTreeNode right;
+
+    public BinaryTreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    
+}
